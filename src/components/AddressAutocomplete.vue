0f65184b6891fb992ffd6d0fc4f850f94f12ee5c
David Xu <thexumaker@berkeley.edu>
2022-04-02 12:11:39 -0700

    feat(typeahead): Use custom typeahead rather than external dependency (#3)

--- src/components/AddressAutocomplete.vue
+++ src/components/AddressAutocomplete.vue
@@ -1,6 +1,34 @@
 <template>
-  <v-select :options="['This is not in a working state yet']"></v-select>
+  <TypeAhead
+	id="typeahead_id"
+	placeholder="Search for an address"
+	:items="addresses"
+	:minInputLength="1"
+	@selectItem="selectItemEventHandler"
+	@onInput="onInputEventHandler"
+>
+</TypeAhead>
 </template>
 
 <script>
+import { defineComponent } from 'vue';
+import TypeAhead from './TypeAhead.vue'
+export default defineComponent({
+  component: {
+    TypeAhead
+  },
+  data() {
+    return {
+      addresses: []
+  }},
+  methods: {
+    selectItemEventHandler(selectedAddress) {
+      console.log(selectedAddress)
+    },
+    onInputEventHandler(input) {
+      console.log(input)
+
+    }
+  }
+})
 </script>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import AddressAutocomplete from './components/AddressAutocomplete.vue'
+import TypeAhead from './components/TypeAhead.vue'
+function install(Vue) {
+  if (install.installed) {
+    return
+  }
+  install.installed = true
+  Vue.component('TypeAhead', TypeAhead)
+}
 
-export default AddressAutocomplete
+const plugin = { install }
+
+// Auto install if Vue is found
+let GlobalVue
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+// Inject install function into component. Allows component to be registered via
+// Vue.use() as well as Vue.component()
+TypeAhead.install = install
+
+export default TypeAhead
