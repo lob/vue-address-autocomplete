f6afbfbab75a1c3ccb0674c3e375fbc3d588d01d
thexumaker <david@Davids-MacBook-Air.local>
2022-04-02 13:46:17 -0700

    woirking typeahead

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <AddressAutocomplete apiKey="live_f1509d4894ea4b21c4568fb87b6fb21d293" />
+</template>
+
+<script>
+import AddressAutocomplete from './components/AddressAutocomplete.vue'
+export default {
+  components: {
+    AddressAutocomplete
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/components/AddressAutocomplete.vue
+++ src/components/AddressAutocomplete.vue
@@ -1,34 +1,90 @@
 <template>
-  <TypeAhead
-	id="typeahead_id"
-	placeholder="Search for an address"
-	:items="addresses"
-	:minInputLength="1"
-	@selectItem="selectItemEventHandler"
-	@onInput="onInputEventHandler"
->
-</TypeAhead>
+	<div class="demo">
+		<h4>Using a strings array</h4>
+		<div class="row">
+			<div class="column column-60">
+				<span class="float-right">
+					<code>({{ addressesFiltered.length }}/{{ addresses.length }})</code>
+				</span>
+				<TypeAhead :items="addresses" :placeholder="options.placeholder" @selectItem="selectItem" @onInput="onInput" @onBlur="onBlur" :minInputLength="options.minInputLength" :itemProjection="
+						(item) => {
+							return item.label;
+						}
+					" />
+			</div>
+			<div class="column column-40">
+				<pre><code>{{data}}</code></pre>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
 import TypeAhead from './TypeAhead.vue'
-export default defineComponent({
-  component: {
+import { postAutocompleteAddress } from './../api'
+export default {
+  components: {
     TypeAhead
   },
-  data() {
-    return {
-      addresses: []
-  }},
-  methods: {
-    selectItemEventHandler(selectedAddress) {
-      console.log(selectedAddress)
-    },
-    onInputEventHandler(input) {
-      console.log(input)
-
+  props: {
+    apiKey: {
+      type: String
     }
-  }
-})
+  },
+	created() {
+		this.addressesFiltered = this.addresses;
+	},
+	data() {
+		return {
+			options: {
+				placeholder: 'Search for an address',
+				minInputLength: 1,
+			},
+			addresses: [],
+			addressesFiltered: [],
+			data: {
+				input: '',
+				selection: null,
+			},
+		};
+	},
+	methods: {
+		selectItem(item) {
+			this.data.selection = item;
+		},
+		async onInput(event) {
+			this.data.selection = null;
+			this.data.input = event.input;
+			this.addressesFiltered = event.items;
+      const newSuggestions = await this.fetchFromAutocompleteAPI(event.input);
+      this.addresses = newSuggestions;
+		},
+    async fetchFromAutocompleteAPI(userInput) {
+      const newAddresses = await postAutocompleteAddress(this.apiKey, userInput)
+      const newAddressJSON = await newAddresses.json()
+      const suggestions = newAddressJSON['suggestions']
+      const newSuggestions = suggestions.map((x) => ({
+          value: x,
+          label: `${x.primary_line} ${x.city} ${x.state}`
+        }))
+      return newSuggestions
+    },
+
+		onBlur(event) {
+			this.data.input = event.input;
+			this.addressesFiltered = event.items;
+		}
+	},
+};
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
